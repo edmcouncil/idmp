611b3e1fe7958f6d2263fb89f6bcf7bd33f235c4
mereolog <pawel.garbacz@makolab.com>
2023-02-06 11:27:52 +0100

    fix to unit test python script - root folder added
    Signed-off-by: mereolog <pawel.garbacz@makolab.com>

--- etc/unit_tests/unit_test_runner.py
+++ etc/unit_tests/unit_test_runner.py
@@ -1,20 +1,21 @@
 import argparse
 import json
+import os.path
 import sys
 
 import yaml
 from rdflib import Graph
 
 
-def get_unit_tests_config(unit_test_config_file_path: str) -> dict:
-    with open(unit_test_config_file_path) as file:
+def get_unit_tests_config(unit_test_config_file_path: str, root_folder: str) -> dict:
+    with open(os.path.join(root_folder, unit_test_config_file_path)) as file:
         config = yaml.safe_load(file)
         file.close()
     return config
 
 
-def get_sparql_query(config: dict) -> str:
-    with open(config['sparql_template_url']) as file:
+def get_sparql_query(config: dict, root_folder: str) -> str:
+    with open(os.path.join(root_folder, config['sparql_template_url'])) as file:
         sparql_template = file.read()
         sparql_parameters = config['parameters']
         file.close()
@@ -22,18 +23,18 @@ def get_sparql_query(config: dict) -> str:
     for sparql_parameter in sparql_parameters:
         sparql_query = sparql_query.replace(sparql_parameter, '"' + config['parameters'][sparql_parameter] + '"')
     return sparql_query
-    
 
-def run_unit_test(ontology_location: str, unit_test_config_file_path: str) -> bool:
+
+def run_unit_test(ontology_location: str, root_folder: str, unit_test_config_file_path: str) -> bool:
     cq_tests_passed = True
     ontology = Graph()
     ontology.parse(ontology_location)
-    unit_tests_config = get_unit_tests_config(unit_test_config_file_path)
+    unit_tests_config = get_unit_tests_config(unit_test_config_file_path, root_folder=root_folder)
     for unit_test_config in unit_tests_config.values():
-        sparql_query = get_sparql_query(config=unit_test_config)
+        sparql_query = get_sparql_query(config=unit_test_config, root_folder=root_folder)
         query_result = ontology.query(sparql_query)
         query_result_as_dict = json.loads(query_result.serialize(format='json').decode())
-        with open(unit_test_config['expected_output_url']) as file:
+        with open(os.path.join(root_folder, unit_test_config['expected_output_url'])) as file:
             expected_query_result = json.loads(file.read())
         if not query_result_as_dict == expected_query_result:
             print('Competency question', unit_test_config['sparql_template_url'], ' run a unit test failed.')
@@ -44,12 +45,15 @@ def run_unit_test(ontology_location: str, unit_test_config_file_path: str) -> bo
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run unit tests')
     parser.add_argument('--ontology_location', help='Path to ontology location')
+    parser.add_argument('--root_folder', help='Path to the root folder')
     args = parser.parse_args()
     
     cq_tests_passed = \
-        run_unit_test(ontology_location=args.ontology_location, unit_test_config_file_path='/configs/unit_tests_config.yaml')
+        run_unit_test(
+            ontology_location=args.ontology_location,
+            root_folder=args.root_folder,
+            unit_test_config_file_path='/configs/unit_tests_config.yaml')
     
     if cq_tests_passed:
         sys.exit(0)
     sys.exit(-1)
-
