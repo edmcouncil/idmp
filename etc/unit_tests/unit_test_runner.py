54e73dbdb72d0c14ec6dd4e3d6b4dd045e774869
mereolog <pawel.garbacz@makolab.com>
2023-02-17 10:52:12 +0100

    Minor typo fixed in unit test messages
    Signed-off-by: mereolog <pawel.garbacz@makolab.com>

--- etc/unit_tests/unit_test_runner.py
+++ etc/unit_tests/unit_test_runner.py
@@ -38,10 +38,10 @@ def run_unit_test(ontology_location: str, root_folder: str, unit_test_config_fil
         with open(os.path.join(root_folder, unit_test_config['expected_output'])) as file:
             expected_query_result = json.loads(file.read())
         if not query_result_as_dict == expected_query_result:
-            print('Competency question', unit_test_config['sparql_template'], ' run a unit test failed.')
+            print('Competency question', unit_test_config['sparql_template'], ' run as a unit test failed.')
             cq_tests_passed = False
         else:
-            print('Competency question', unit_test_config['sparql_template'], ' run a unit test passed.')
+            print('Competency question', unit_test_config['sparql_template'], ' run as a unit test passed.')
     return cq_tests_passed
 
 
