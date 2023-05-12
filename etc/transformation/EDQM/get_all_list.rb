9aca4ed14c606f611ebab22e459c1038c44049ab
Ali Ariff <ali.ariff@accurids.com>
2023-05-12 12:30:17 +0200

    Update after chatgpt
    Signed-off-by: Ali Ariff <ali.ariff@accurids.com>

--- etc/transformation/EDQM/get_all_list.rb
+++ etc/transformation/EDQM/get_all_list.rb
@@ -5,45 +5,66 @@ require "json"
 require "openssl"
 require "base64"
 
+# Set credentials
 EMAIL = "<!!! CHANGE ME !!!>"
 KEY = "<!!! CHANGE ME !!!>"
 
-def perform_request(url)
+def prepare_request(url)
   uri = URI(url)
   request = Net::HTTP::Get.new(uri)
 
-  # specify Date header
+  # Specify Date header
   date_now = Time.now.utc.strftime("%a, %d %b %Y %H:%M:%S GMT")
   request["Date"] = date_now
 
-  # prepare & set the authorization header
+  # Prepare & set the authorization header
   string_to_sign = "GET&#{uri.path}&#{uri.host}&#{date_now}"
   hash = OpenSSL::HMAC.digest("sha512", KEY, string_to_sign)
   encoded = Base64.strict_encode64(hash)
   signature = encoded.slice(-22, 22)
-  # puts string_to_sign, hash, encoded, signature
   request["X-STAPI-KEY"] = "#{EMAIL}|#{signature}"
 
-  # perform request
+  [uri, request]
+end
+
+def perform_request(url)
+  uri, request = prepare_request(url)
+
+  # Perform request
   response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
     http.request(request)
   end
 
-  # parse response to json
-  JSON.parse(response.body)
-end
-
-klasses = perform_request("https://standardterms.edqm.eu/standardterms/api/v1/classes")
-directory_name = "lists"
-Dir.mkdir(directory_name) unless File.exist?(directory_name)
-klasses["content"].each do |klass|
-  code = klass["code"]
-  url = "https://standardterms.edqm.eu/standardterms/api/v1/full_data_by_class/#{code}/1/1"
-  data = perform_request(url)
-  p "writing #{code} list"
-  File.open("#{directory_name}/EDQM-#{code}.json", "w") do |f|
-    f.write(JSON.pretty_generate(data["content"]))
+  # Parse response to json
+  begin
+    JSON.parse(response.body)
+  rescue JSON::ParserError => e
+    puts "Failed to parse JSON response: #{e.message}"
+    return nil
   end
 end
 
-puts "All data available under ./#{directory_name}/ directory"
+def write_to_file(directory_name, code, data)
+  begin
+    File.write("#{directory_name}/EDQM-#{code}.json", JSON.pretty_generate(data["content"]))
+  rescue => e
+    puts "Failed to write to file: #{e.message}"
+  end
+end
+
+# Main script
+begin
+  klasses = perform_request("https://standardterms.edqm.eu/standardterms/api/v1/classes")
+  directory_name = "lists"
+  Dir.mkdir(directory_name) unless File.exist?(directory_name)
+  klasses["content"].each do |klass|
+    code = klass["code"]
+    url = "https://standardterms.edqm.eu/standardterms/api/v1/full_data_by_class/#{code}/1/1"
+    data = perform_request(url)
+    puts "writing #{code} list"
+    write_to_file(directory_name, code, data) if data
+  end
+  puts "All data available under ./#{directory_name}/ directory"
+rescue => e
+  puts "An error occurred: #{e.message}"
+end
