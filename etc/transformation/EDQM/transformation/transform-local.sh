1c6688780075f6f0128a3428f33d5f8fce2aa7a2
konradkrebs1411 <konrad.krebs@accurids.com>
2023-07-24 13:42:37 +0200

    Typo

--- etc/transformation/EDQM/transformation/transform-local.sh
+++ etc/transformation/EDQM/transformation/transform-local.sh
@@ -9,7 +9,7 @@ echo "Transforming $query"
 $docker_cmd $java_docker_image sh -c "java -Xmx20g \
     -jar sparql-generate-$sparql_generate_version.jar \
     -l INFO \
-    -q generic_transform.rqg \
+    -q generic_transformation.rqg \
     -o $query.ttl \
     -fo TTL"
 echo "Result available in $query.ttl"
