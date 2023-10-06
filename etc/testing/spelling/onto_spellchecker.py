ffe73d2fa9f530bb4e6b6cafc945dccaa363f763
mereolog <pawel.garbacz@makolab.com>
2023-10-06 12:48:41 +0200

    new script and spell list
    Signed-off-by: mereolog <pawel.garbacz@makolab.com>

--- etc/testing/spelling/onto_spellchecker.py
+++ etc/testing/spelling/onto_spellchecker.py
@@ -4,35 +4,41 @@ import logging
 import re
 import sys
 
-from rdflib import Graph, Literal
+from rdflib import Graph, Literal, URIRef
 from spellchecker import SpellChecker
 
-# ONTOLOGY_IRI_FILTER = 'spec.pistoiaalliance.org'
 HTTP_REGEX = re.compile(pattern='http[^\s]+')
+LOCAL_NAME_SEPARATOR = '/'
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Run spelling check')
-    parser.add_argument('--spell', help='Path to file with new spells', metavar='SPELL')
-    parser.add_argument('--ontology', help='Path to ontology file', metavar='ONTOLOGY')
-    parser.add_argument('--filter', help='IRI filter', metavar='FILTER')
-    args = parser.parse_args()
-    
-    logging.basicConfig(
-        format='%(message)s',
-        filename='spellcheck_log.log')
-    
+
+def __get_local_name_from_iri(iri: URIRef) -> str:
+    local_name = str(iri).split(sep=LOCAL_NAME_SEPARATOR)[-1]
+    return local_name
+
+
+def check_spelling_in_ontology(new_spell_file_path: str, ontology_location: str, resource_filter: str):
     spell = SpellChecker()
-    with open(file=args.spell) as new_spell_file:
+    with open(file=new_spell_file_path) as new_spell_file:
         new_spell_words = json.load(new_spell_file)
     spell.word_frequency.load_words(new_spell_words)
     
     ontology = Graph()
-    ontology.parse(args.ontology)
+    ontology.parse(ontology_location)
+    
+    local_names_in_ontology = set()
+    for (subject, predicate, object) in ontology:
+        if isinstance(subject, URIRef):
+            local_names_in_ontology.add(__get_local_name_from_iri(iri=subject))
+        if isinstance(predicate, URIRef):
+            local_names_in_ontology.add(__get_local_name_from_iri(iri=predicate))
+        if isinstance(object, URIRef):
+            local_names_in_ontology.add(__get_local_name_from_iri(iri=object))
+    spell.word_frequency.load_words(local_names_in_ontology)
     
     misspelled_words_in_triples = set()
-    
+    misspelled_words = set()
     for (subject, predicate, object) in ontology:
-        if args.filter in str(subject):
+        if resource_filter in str(subject):
             if isinstance(object, Literal):
                 literal = object
                 literal_value = literal.value
@@ -41,6 +47,7 @@ if __name__ == "__main__":
                         if literal.language != 'en':
                             continue
                     trunc_text = re.sub(pattern=HTTP_REGEX, repl='', string=literal_value)
+                    trunc_text = re.sub(pattern=r'[a-z\-]+:[^\s]+', repl='', string=trunc_text)
                     words = spell.split_words(text=trunc_text)
                     for word in words:
                         word = word.replace("'s", "")
@@ -50,7 +57,7 @@ if __name__ == "__main__":
                             if word.endswith('s'):
                                 if word[:-1] == word[:-1].upper():
                                     continue
-                        if word[0].isnumeric():
+                        if any(char.isdigit() for char in word):
                             continue
                         if word[0].upper() == word[0]:
                             continue
@@ -58,6 +65,9 @@ if __name__ == "__main__":
                             if len(word) > 2:
                                 triple = '|'.join([subject, predicate, object])
                                 misspelled_words_in_triples.add('|'.join([word, triple]))
+                                if word not in misspelled_words:
+                                    print(word, 'is possibly misspelled.')
+                                misspelled_words.add(word)
     
     misspelled_words_in_triples = list(misspelled_words_in_triples)
     misspelled_words_in_triples.sort()
@@ -67,3 +77,22 @@ if __name__ == "__main__":
     if len(misspelled_words_in_triples) > 0:
         print('Possible spelling errors found - for the details consult spellcheck_log.log file')
         sys.exit(1)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Run spelling check')
+    parser.add_argument('--spell', help='Path to file with new spells', metavar='SPELL')
+    parser.add_argument('--ontology', help='Path to ontology file', metavar='ONTOLOGY')
+    parser.add_argument('--filter', help='IRI filter', metavar='FILTER')
+    args = parser.parse_args()
+
+    logging.basicConfig(
+        format='%(message)s',
+        filename='spellcheck_log.log')
+
+    check_spelling_in_ontology(new_spell_file_path=args.spell, ontology_location=args.ontology, resource_filter=args.filter)
+
+    
+# check_spelling_in_ontology(
+#     new_spell_file_path='spell.json',
+#     ontology_location='https://spec.pistoiaalliance.org/idmp/ontology/master/latest/QuickIDMPDev.ttl',
+#     resource_filter='spec.pistoiaalliance.org')
