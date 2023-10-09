37a95b3b0e82cdf8db73c7e8a0c0d5d7e75fe404
mereolog <pawel.garbacz@makolab.com>
2023-10-09 09:55:01 +0200

    further code refactoring
    Signed-off-by: mereolog <pawel.garbacz@makolab.com>

--- etc/testing/spelling/onto_spellchecker.py
+++ etc/testing/spelling/onto_spellchecker.py
@@ -28,13 +28,41 @@ def __get_local_names(ontology: Graph) -> set:
     return local_names_in_ontology
 
 
-def __get_misspellings(ontology: Graph, resource_filter: str, spell) -> set():
+def __check_word(
+        word: str,
+        misspelled_words_in_triples: set,
+        misspelled_words: set,
+        spellchecker: SpellChecker,
+        triple_subject: URIRef,
+        triple_predicate: URIRef):
+    word = word.replace("'s", "")
+    if word == word.upper():
+        return
+    else:
+        if word.endswith('s'):
+            if word[:-1] == word[:-1].upper():
+                return
+    if any(char.isdigit() for char in word):
+        return
+    if word[0].upper() == word[0]:
+        return
+    
+    if word not in spellchecker and word[:-1] not in spellchecker and not word.lower() in spellchecker:
+        if len(word) > 2:
+            triple = '|'.join([triple_subject, triple_predicate, object])
+            misspelled_words_in_triples.add('|'.join([word, triple]))
+            if word not in misspelled_words:
+                print(word, 'is possibly misspelled.')
+            misspelled_words.add(word)
+
+
+def __get_misspellings(ontology: Graph, resource_filter: str, spellchecker: SpellChecker) -> set:
     misspelled_words_in_triples = set()
     misspelled_words = set()
-    for (subject, predicate, object) in ontology:
-        if resource_filter in str(subject):
-            if isinstance(object, Literal):
-                literal = object
+    for (triple_subject, triple_predicate, triple_object) in ontology:
+        if resource_filter in str(triple_subject):
+            if isinstance(triple_object, Literal):
+                literal = triple_object
                 literal_value = literal.value
                 if isinstance(literal_value, str):
                     if literal.language is not None:
@@ -42,26 +70,15 @@ def __get_misspellings(ontology: Graph, resource_filter: str, spell) -> set():
                             continue
                     trunc_text = re.sub(pattern=HTTP_REGEX, repl='', string=literal_value)
                     trunc_text = re.sub(pattern=r'[a-z\-]+:[^\s]+', repl='', string=trunc_text)
-                    words = spell.split_words(text=trunc_text)
+                    words = spellchecker.split_words(text=trunc_text)
                     for word in words:
-                        word = word.replace("'s", "")
-                        if word == word.upper():
-                            continue
-                        else:
-                            if word.endswith('s'):
-                                if word[:-1] == word[:-1].upper():
-                                    continue
-                        if any(char.isdigit() for char in word):
-                            continue
-                        if word[0].upper() == word[0]:
-                            continue
-                        if word not in spell and word[:-1] not in spell and not word.lower() in spell:
-                            if len(word) > 2:
-                                triple = '|'.join([subject, predicate, object])
-                                misspelled_words_in_triples.add('|'.join([word, triple]))
-                                if word not in misspelled_words:
-                                    print(word, 'is possibly misspelled.')
-                                misspelled_words.add(word)
+                        __check_word(
+                            word=word,
+                            misspelled_words_in_triples=misspelled_words_in_triples,
+                            misspelled_words=misspelled_words,
+                            spellchecker=spellchecker,
+                            triple_subject=triple_subject,
+                            triple_predicate=triple_predicate)
     return misspelled_words_in_triples
 
 
@@ -69,19 +86,23 @@ def check_spelling_in_ontology(new_spell_file_path: str, ontology_location: str,
     spell = SpellChecker()
     with open(file=new_spell_file_path) as new_spell_file:
         new_spell_words = json.load(new_spell_file)
-    spell.word_frequency.load_words(new_spell_words)
+        spell.word_frequency.load_words(new_spell_words)
     ontology = Graph()
     ontology.parse(ontology_location)
     local_names_in_ontology = __get_local_names(ontology=ontology)
     spell.word_frequency.load_words(local_names_in_ontology)
-    misspelled_words_in_triples = __get_misspellings(ontology=ontology, spell=spell, resource_filter=resource_filter)
+    misspelled_words_in_triples = \
+        __get_misspellings(
+            ontology=ontology,
+            spellchecker=spell,
+            resource_filter=resource_filter)
     misspelled_words_in_triples = list(misspelled_words_in_triples)
     misspelled_words_in_triples.sort()
     for word_in_triple in misspelled_words_in_triples:
         logging.warning(msg=word_in_triple)
     
     if len(misspelled_words_in_triples) > 0:
-        print('Possible spelling errors found - for the details consult spellcheck_log.log file')
+        print('Possible spelling errors found - for the details consult spellcheck_errors.log file')
         sys.exit(1)
 
 
@@ -94,7 +115,7 @@ if __name__ == "__main__":
     
     logging.basicConfig(
         format='%(message)s',
-        filename='spellcheck_log.log')
+        filename='spellcheck_errors.log')
     
     check_spelling_in_ontology(
         new_spell_file_path=args.spell,
